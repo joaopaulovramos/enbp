ff03c72e834ab5335a7ffe4738209b3d2aefddcf
Braully Rocha <braully@gmail.com>
2023-08-02 06:54:41 -0300

    ajuste de filtros

--- djangosige/apps/janela_unica/admin.py
+++ djangosige/apps/janela_unica/admin.py
@@ -64,10 +64,10 @@ class DocumentoUnicoFinanceiroAdmin(FSMTransitionMixin, SimpleHistoryAdmin):
     # Desabilita as ações em massa
     actions = None
     # Atributos de filtragem
-    list_filter = ('situacao', 'data_inclusao')
+    list_filter = ('situacao', 'data_inclusao', 'projeto', 'plano_conta')
     search_fields = ('situacao',)
     # Atributos da tabela
-    list_display = ('pk', 'descricao', 'situacao', 'data_inclusao', 'data_finalizacao', 'tipo_arquivo', 'numero', 'responsavel', 'fornecedor', 'valor_total',)
+    list_display = ('pk', 'descricao', 'situacao', 'data_inclusao', 'data_finalizacao', 'tipo_arquivo', 'numero', 'responsavel', 'fornecedor', 'projeto', 'plano_conta', 'valor_total',)
 
     history_list_display = ["changed_fields", "list_changes"]
 
