197ee4864ba6137e2c309686945af3f7c14f2331
Thiago Fernandes <thiago.sousa@ifg.edu.br>
2023-08-21 10:23:01 -0300

    correção de código

--- djangosige/apps/janela_unica/menus.py
+++ djangosige/apps/janela_unica/menus.py
@@ -5,7 +5,7 @@ janela_unica_children = [
     MenuItem("Caixa de entrada", reverse("janela_unica:caixaentrada"), weight=10, icon="user"),
     MenuItem("Contratos", '/admin/janela_unica/contrato/', weight=20, icon="user", check=lambda r: r.user.has_perm('janela_unica.view_contrato')),
     MenuItem("Documentos", '/admin/janela_unica/documentounicofinanceiro/', weight=20, icon="user"),
-    MenuItem("Adicionar", reverse("janela_unica:adicionar_documento_unico"), weight=20, icon="user"),
+    #MenuItem("Adicionar", reverse("janela_unica:adicionar_documento_unico"), weight=20, icon="user"),
 ]
 
 Menu.add_item("main", MenuItem("Janela Única", '#',
